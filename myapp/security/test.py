99480269503d12f9a3b2faf624ba6a5fa039a709
singhritik09 <singhritik2711@gmail.com>
2026-02-10 01:14:33 +0530

    Kafka producer

---
+++ myapp/security/test.py
@@ -0,0 +1,16 @@
+from kafka import KafkaProducer
+import json
+import time
+
+producer= KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+message={
+    "type":"health_check",
+    "message":"Application is running",
+    "timestamp": time.time(),
+    "status":"ok"
+    }
+producer.send('traffic_snapshots', message)
+producer.flush()
+
+print("Health check message sent to Kafka topic 'traffic_snapshots'")
